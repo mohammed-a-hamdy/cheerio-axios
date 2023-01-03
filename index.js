7c09c9a5ba59aa703b0e1779ffcad747441666b4
Mohammed <heroboy107@gmail.com>
2023-01-03 12:25:35 +0200

    cute

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 const cron = require('node-cron');
+const fs = require('fs');
 
 const loadPage = async () => {
     const response = await axios.get("https://www.cbe.org.eg/en/EconomicResearch/Statistics/Pages/ExchangeRatesListing.aspx");
@@ -13,7 +14,18 @@ const loadPage = async () => {
     });
     
    const day = $('.date').text();
+
    console.log(day,'Buy: ',bag[1],'Sell: ',bag[2]);
+   const stdOutConmsole = day+',Buy: '+bag[1]+',Sell: '+bag[2];
+   
+    if (!fs.existsSync('output.csv')) {
+        fs.writeFileSync('output.csv', 'Date,Buy,Sell');
+        fs.writeFileSync('output.csv', stdOutConmsole+'\n');
+      } else {
+        fs.appendFileSync('output.csv', stdOutConmsole+'\n');
+      }
+    console.log(`Output written to output.txt`);
+ 
 
 };
 
